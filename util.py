046b158e23d9d40aaaf051073def1d8a69d25dd0
spencersweeney <101937329+spencersweeney@users.noreply.github.com>
2022-03-28 13:17:47 -0400

    Add files via upload

---
+++ util.py
@@ -0,0 +1,25 @@
+# Util functions
+import string
+
+
+def similarity_title(str1, str2) -> int:
+    clean_text1 = ''.join([char.lower() for char in str1 if char not in string.punctuation])
+    clean_text2 = ''.join([char.lower() for char in str2 if char not in string.punctuation])
+
+    words_in_str1 = set(clean_text1.split())
+    words_in_str2 = set(clean_text2.split())
+
+    union = words_in_str1 & words_in_str2
+
+    return int((len(union) / min(len(words_in_str1), len(words_in_str2))) * 100)
+
+
+# print(similarity_title('Hello World', 'Hello World'))
+# print(similarity_title('Hello World', 'hello world'))
+# print(similarity_title('Hello World', 'Hello World Spencer'))
+#
+# print(similarity_title('Today Russia attacked the Ukraine, by attacking Kyiv',
+#                  'The War in Russia has escalated with Russia bombing Kyiv'))
+#
+# print(similarity_title('Zelensky says Ukraine is ready to Discuss Neutrality',
+#                  'Ukraine, Russia prepare to talk as Missiles Hit Cities'))
